eb99614c8780f9fe11fea2355b8ac03f6a026bf6
joaqu1m <joaquim.pires@sptech.school>
2023-09-16 12:29:27 -0300

    remoçao bun.build

--- index.ts
+++ index.ts
@@ -1,12 +1,5 @@
-await Bun.build({
-    entrypoints: ["./index.ts"],
-    outdir: "./build",
-    minify: true,
-    plugins: []
-})
-
 Bun.serve({
-    port: 443,
+    port: 80,
     fetch(req: any) {
         return new Response("Hello World!")
     }
